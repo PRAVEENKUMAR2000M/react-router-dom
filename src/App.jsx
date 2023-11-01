57d6523b7ae9a7bfcb6b714f02f70b870c0342a7
PRAVEENKUMAR M <131171871+PRAVEENKUMAR2000M@users.noreply.github.com>
2023-11-01 20:21:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+
+import React, { createContext, useState } from 'react';
+import ChildComponent from './ChildComponent';
+
+
+const messageContext = createContext();
 
 function App() {
+  const [message, setmessage] = useState("Hello child");
+
   return (
-    <div>App</div>
+    <div>
+      <h1>parentcomponent</h1>
+      <p>hello message from parentcomponent{message}</p>
+      <messageContext.Provider value={message}>
+        <ChildComponent />
+      </messageContext.Provider>
+    </div>
   )
 }
 
-export default App
+export { App as default, messageContext };
+
+
+
+//childcomponent.jsx
+
+import React, { createContext, useContext, useState } from 'react'
+
+import Grandchildcomponent from './Grandchildcomponent';
+const TextContext = createContext();
+function ChildComponent() {
+
+    const [text, settext] = useState("Hello from indrajith");
+    // const TextContext = "who iam"
+    return (
+        <div>
+            <h1>ChildComponent</h1>
+            <p>message from child </p>
+            <TextContext.Provider value={text}>
+                <Grandchildcomponent />
+            </TextContext.Provider>
+
+        </div>
+    )
+}
+
+export { ChildComponent as default, TextContext }
+
+
+//Grandchildcomponent.jsx
+
+import React, { useContext } from 'react';
+import { TextContext } from './ChildComponent';
+
+function Grandchildcomponent() {
+    const text = useContext(TextContext);
+    
+
+    return (
+        <div>
+            <h1>Grandchildcomponent</h1>
+            <p>message from Grandchildcomponent: {text}</p>
+        </div>
+    );
+}
+
+export default Grandchildcomponent;
